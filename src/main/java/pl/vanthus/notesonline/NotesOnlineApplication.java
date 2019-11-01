8838a3defa12fc46376061a65fb406dae4368237
Bartlomiej Lazarek <bartek.lazarek@gmail.com>
2019-11-01 21:10:20 +0100

    init commit

---
+++ src/main/java/pl/vanthus/notesonline/NotesOnlineApplication.java
@@ -0,0 +1,13 @@
+package pl.vanthus.notesonline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotesOnlineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotesOnlineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/vanthus/notesonline/NotesOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.vanthus.notesonline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotesOnlineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
