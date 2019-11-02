acb087802dba584edccff01bc79c48b935761765
Bartlomiej Lazarek <bartek.lazarek@gmail.com>
2019-11-02 10:07:13 +0100

    Added repositories

---
+++ src/main/java/pl/vanthus/notesonline/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package pl.vanthus.notesonline.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import pl.vanthus.notesonline.model.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
